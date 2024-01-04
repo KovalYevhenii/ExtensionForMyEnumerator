dc96e9c45ea6243fda338ba4fe6b40ab5295715a
KovalYevhenii <kovalyevhenii@gmail.com>
2024-01-04 16:12:41 +0100

    update

--- ExtensionForGetMaxMethod/FilesSearch/FileFoundArgs.cs
+++ ExtensionForGetMaxMethod/FilesSearch/FileFoundArgs.cs
@@ -4,5 +4,4 @@ internal class FileFoundArgs : EventArgs
     public string FoundFile { get; }
     public bool CancelRequested { get; set; }
     public FileFoundArgs(string fileName) => FoundFile = fileName;
-
 }
